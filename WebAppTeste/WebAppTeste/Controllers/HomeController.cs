d445f7deccf17d434f63cab755b7943bfaf6d7b9
Cristiano Serafim Lauffer <cristiano.serafim.lauffer@gmail.com>
2017-06-03 02:36:35 -0300

    0.1-version
    Versao 0.1

---
+++ WebAppTeste/WebAppTeste/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using WebAppTeste.Models;
+using System.Text;
+
+namespace WebAppTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Teste()
+        {
+            List<RedesLivresPoa> redesLivresPoa;
+
+            using (var httpClient = new HttpClient())
+            {
+                var json = await httpClient.GetStringAsync("http://www.portoalegrelivre.com.br/php/services/WSPoaLivreRedes.php");
+
+                JsonSerializer serializer = new JsonSerializer();
+                redesLivresPoa = JsonConvert.DeserializeObject<List<RedesLivresPoa>>(json);
+                redesLivresPoa = redesLivresPoa.OrderBy(RedesLivresPoa => RedesLivresPoa.Empresa).ToList();
+            }
+
+            return View(redesLivresPoa);
+        }
+    }
+}
---
+++ WebAppTeste/WebAppTeste/Models/RedesLivresPoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTeste.Models
+{
+    public class RedesLivresPoa
+    {
+        public string Empresa { get; set; }
+        public string Site { get; set; }
+        public string Logomarca { get; set; }
+        public string NomeRede { get; set; }
+        public string Endereco { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Setor { get; set; }
+    }
+}
